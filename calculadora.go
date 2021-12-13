dbc1cf67806d1a2165cbcdc8ae6a0f988e8d3503
AngelDev996 <angeldev996@gmail.com>
2021-12-13 02:59:09 -0600

    intro a errores

--- calculadora.go
+++ calculadora.go
@@ -16,7 +16,14 @@ func main() {
 	valores := strings.Split(operacion, "+")
 	fmt.Println(valores)
 	fmt.Println(valores[0] + valores[1])
-	operador1, _ := strconv.Atoi(valores[0])
-	operador2, _ := strconv.Atoi(valores[1])
-	fmt.Println(operador1 + operador2)
+	operador1, err1 := strconv.Atoi(valores[0]) //Atoi castea string a enteros
+	operador2, err2 := strconv.Atoi(valores[1])
+
+	if err1 != nil || err2 != nil {
+		fmt.Println("Hubo un error")
+
+	} else {
+		fmt.Println(operador1 + operador2)
+	}
+
 }
