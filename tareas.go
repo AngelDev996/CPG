7424700f495bb5917d8e8bd77ad84c3b7f6aa33a
AngelDev996 <angeldev996@gmail.com>
2021-12-13 05:10:36 -0600

    creacion de nuevo programa (tareas.go)

---
+++ tareas.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type task struct {
+	nombre      string
+	descripcion string
+	completado  bool
+}
+
+func (t task) marcarCompleta() {
+	t.completado = true
+}
+
+func (t task) actualizarDescripcion(descripcion string) {
+	t.descripcion = descripcion
+}
+
+func (t task) actualizarNombre(nombre string) {
+	t.nombre = nombre
+}
+func main() {
+	t := task{
+		nombre:      "Completar curso",
+		descripcion: "Completacion de curso",
+	}
+	// fmt.Println(t)
+	fmt.Printf("%+v\n", t)
+	t.marcarCompleta()
+	t.actualizarNombre("Otro curso")
+	t.actualizarDescripcion("Otra descripcion")
+	fmt.Printf("%+v\n", t)
+
+}
