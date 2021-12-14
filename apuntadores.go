e697df724089a8c07d4528c5fd49b8721e33b91b
AngelDev996 <angeldev996@gmail.com>
2021-12-14 00:01:08 -0600

    pointer

---
+++ apuntadores.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	x := 25
+	fmt.Println(x)
+	// cambiarValor(x)
+	fmt.Println(&x)
+	// cambiarValor(x)
+	y := &x
+	fmt.Println(y)
+
+}
+
+func cambiarValor(a int) {
+	fmt.Println(&a)
+	a = 30
+}
